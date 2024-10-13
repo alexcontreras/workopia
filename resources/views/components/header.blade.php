eb9cd86761d3b89122a87b9698a68b2ccd42fa6f
Alex Contreras <rvigilc@gmail.com>
2024-10-12 22:22:53 -0600

    38. Component attributes & props

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,28 +4,12 @@
             <a href="{{url('/')}}">Workopia</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
-            <a href="{{url('/jobs')}}" class="text-white hover:underline py-2 {{request()->is('jobs') ? 'text-yellow-500 font-bold' : ''}}"
-                >All Jobs</a
-            >
-            <a
-                href="{{url('/jobs/saved')}}"
-                class="text-white hover:underline py-2 {{request()->is('jobs/saved') ? 'text-yellow-500 font-bold' : ''}}"
-                >Saved Jobs</a
-            >
-            <a href="{{url('/login')}}" class="text-white hover:underline py-2 {{request()->is('login') ? 'text-yellow-500 font-bold' : ''}}"
-                >Login</a
-            >
-            <a
-                href="{{url('/register')}}"
-                class="text-white hover:underline py-2 {{request()->is('register') ? 'text-yellow-500 font-bold' : ''}}"
-                >Register</a
-            >
-            <a
-                href="{{url('/dashboard')}}"
-                class="text-white hover:underline py-2 {{request()->is('dashboard') ? 'text-yellow-500 font-bold' : ''}}"
-            >
-                <i class="fa fa-gauge mr-1"></i> Dashboard
-            </a>
+            <x-nav-link url="/" title="Homepage" :active="request()->is('/') ? 'text-yellow-500 font-bold' : ''">Home</x-nav-link>
+            <x-nav-link url="/jobs" title="All Jobs" :active="request()->is('/jobs') ? 'text-yellow-500 font-bold' : ''">All Jobs</x-nav-link>
+            <x-nav-link url="/jobs/saved" title="Saved Jobs" :active="request()->is('/jobs/saved') ? 'text-yellow-500 font-bold' : ''">Saved Jobs</x-nav-link>
+            <x-nav-link url="/login" title="Login" :active="request()->is('/login') ? 'text-yellow-500 font-bold' : ''">Login</x-nav-link>
+            <x-nav-link url="/register" title="Register" :active="request()->is('/register') ? 'text-yellow-500 font-bold' : ''">Register</x-nav-link>
+            <x-nav-link url="/dashboard" title="Dashboard" :active="request()->is('/dashboard') ? 'text-yellow-500 font-bold' : ''" icon="gauge">Dashboard</x-nav-link>
             <a
                 href="{{url('/jobs/create')}}"
                 class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
