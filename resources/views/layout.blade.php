fd719a3483618e5d7b5abc25826758c211a7d7ba
Alex Contreras <rvigilc@gmail.com>
2024-10-20 23:57:05 -0600

    77. Flash messages & alert component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,8 +8,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 
@@ -20,6 +21,14 @@
         <x-top-banner />
     @endif
     <main class="container mx-auto p-4 m-4">
+        {{-- Display alert messages --}}
+        @if (session('success'))
+            <x-alert type="success" message="{{ session('success') }}" />
+        @endif
+
+        @if (session('error'))
+            <x-alert type="error" message="{{ session('error') }}" />
+        @endif
         {{ $slot }}
     </main>
     <script src="{{ asset('js/script.js') }}"></script>
