05fb7698eeaf14bc879a92701012ff0af1ed9d05
Alex Contreras <rvigilc@gmail.com>
2024-10-21 00:05:15 -0600

    72. Alpine.js & alert dismiss

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-blue-900 text-white p-4">
+<header class="bg-blue-900 text-white p-4" x-data="{ open: false }">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-3xl font-semibold">
             <a href="{{ url('/') }}">Workopia</a>
@@ -12,12 +12,13 @@
             <x-nav-link url="/dashboard" title="Dashboard" :active="request()->is('dashboard') ? 'text-yellow-500 font-bold' : ''" icon="gauge">Dashboard</x-nav-link>
             <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
         </nav>
-        <button id="hamburger" class="text-white md:hidden flex items-center">
+        <button @click="open = !open" id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
         </button>
     </div>
     <!-- Mobile Menu -->
-    <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+    <nav x-show="open" @click.away="open = false" id="mobile-menu"
+        class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <x-nav-link url="/jobs" title="All Jobs" :active="request()->is('jobs') ? 'text-yellow-500 font-bold' : ''" :mobile="true">All Jobs</x-nav-link>
         <x-nav-link url="/jobs/saved" title="Saved Jobs" :active="request()->is('jobs/saved') ? 'text-yellow-500 font-bold' : ''" :mobile="true">Saved Jobs</x-nav-link>
         <x-nav-link url="/login" title="Login" :active="request()->is('login') ? 'text-yellow-500 font-bold' : ''" :mobile="true">Login</x-nav-link>
